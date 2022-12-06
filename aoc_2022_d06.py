c4b51aa8b45a995e489b189c7569e7749c3162bf
Rastislav Svoboda <rastislav.svoboda@gmail.com>
2022-12-06 06:45:25 +0100

    day6 - run sample+puzzle

--- aoc_2022_d06.py
+++ aoc_2022_d06.py
@@ -1,8 +1,12 @@
 from datetime import datetime
 
+CRED = '\033[91m'
+CGRN = '\033[92m'
+CEND = '\033[0m'
+
 start = datetime.now()
-lines = open('6.in').readlines()
-lines = open('6.ex1').readlines()
+lines_sample = open('6.ex1').readlines()
+lines_puzzle = open('6.in').readlines()
 
 
 def solve(lines, part):
@@ -24,8 +28,10 @@ def solve(lines, part):
     return res
 
 
-print(solve(lines, 1))  # 1109
-print(solve(lines, 2))  # 3965
+print(CRED + "sample:", solve(lines_sample, 1), CEND)  # 7
+print(CGRN + "puzzle:", solve(lines_puzzle, 1), CEND)  # 1109
+print(CRED + "sample:", solve(lines_sample, 2), CEND)  # 19
+print(CGRN + "puzzle:", solve(lines_puzzle, 2), CEND)  # 3965
 
 stop = datetime.now()
 print("duration:", stop - start)
