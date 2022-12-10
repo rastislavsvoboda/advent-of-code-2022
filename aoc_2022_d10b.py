629b13db59f0bf1d3450067da3bd30a46e3d7c7a
Rastislav Svoboda <rastislav.svoboda@gmail.com>
2022-12-10 08:41:15 +0100

    day10 fix and other

--- aoc_2022_d10b.py
+++ aoc_2022_d10b.py
@@ -9,74 +9,66 @@ start = datetime.now()
 lines_sample = open('10.ex1').readlines()
 lines_puzzle = open('10.in').readlines()
 
+R = 40
 
-def check1(c, x, S, V):
+def check1(c, x, V):
     c += 1
-    if c in S:
+    if (c - 20) % R == 0:
         V.append(c * x)
     return c
 
 
 def solve1(lines):
-    S = [20, 60, 100, 140, 180, 220]
     V = []
-    res = 0
     x = 1
     c = 1
     for line in lines:
         words = line.strip().split()
         if words[0] == "noop":
-            c = check1(c, x, S, V)
+            c = check1(c, x, V)
         elif words[0] == "addx":
             var = int(words[1])
-            c = check1(c, x, S, V)
+            c = check1(c, x, V)
             x += var
-            c = check1(c, x, S, V)
+            c = check1(c, x, V)
     res = sum(V)
-
     return res
 
 
-def check2(c, x, p, P):
-    i = (c-1) % 40
-
+def check2(c, x, P):
+    i = (c-1) % R
     if abs(x - i) <= 1:
-        p += "█"
+        P += "█"
     else:
-        p += " "
-    if len(p) == 40:
-        P.append(p)
-        p = ""
+        P += " "
     c += 1
-
-    return c, p
+    return c
 
 
 def solve2(lines):
     P = []
-    p = ""
     x = 1
     c = 1
     for line in lines:
         words = line.strip().split()
         if words[0] == "noop":
-            c, p = check2(c, x, p, P)
+            c = check2(c, x, P)
         elif words[0] == "addx":
-            c, p = check2(c, x, p, P)
+            c = check2(c, x, P)
             var = int(words[1])
-            c, p = check2(c, x, p, P)
+            c = check2(c, x, P)
             x += var
 
-    for p in P:
-        print(p)
+    for p in range(0, len(P), R):
+        print("".join(P[p:p+R]))
 
     return None
 
 
 print(CRED + "sample:", solve1(lines_sample), CEND)  # 13140
 print(CGRN + "puzzle:", solve1(lines_puzzle), CEND)  # 14060
-# print(CRED + "sample:", solve2(lines_sample), CEND)  #
-# print(CGRN + "puzzle:", solve2(lines_puzzle), CEND)  # PAPKFKEJ
+print(CRED + "sample:", solve2(lines_sample), CEND)  #
+print(CGRN + "puzzle:", solve2(lines_puzzle), CEND)  # PAPKFKEJ
 
 
 stop = datetime.now()
